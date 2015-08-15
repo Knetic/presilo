312e44d3ee099af650772372e03f81f5eb9c0170
George Lester <glester491@gmail.com>
2015-08-15 00:00:00 -0700

    Implemented basic setters/getters

--- src/presilo/codegen_java.go
+++ src/presilo/codegen_java.go
@@ -94,9 +94,118 @@ func generateJavaConstructor(schema *ObjectSchema) string {
 func generateJavaFunctions(schema *ObjectSchema) string {
 
   var ret bytes.Buffer
+  var subschema TypeSchema
+  var toWrite string
+  var propertyName, properName, camelName, typeName string
+
+  for propertyName, subschema = range schema.Properties {
+
+    properName = ToJavaCase(propertyName)
+    camelName = ToCamelCase(propertyName)
+    typeName = generateJavaTypeForSchema(subschema)
+
+    // getter
+    toWrite = fmt.Sprintf("\n\tpublic %s get%s()\n\t{", typeName, camelName)
+    ret.WriteString(toWrite)
+
+    toWrite = fmt.Sprintf("\n\t\treturn this.%s;\n\t}", properName)
+    ret.WriteString(toWrite)
+
+    // setter
+    toWrite = fmt.Sprintf("\n\tpublic void set%s(%s value)\n\t{", camelName, typeName)
+    ret.WriteString(toWrite)
+
+    switch subschema.GetSchemaType() {
+    case SCHEMATYPE_BOOLEAN:
+      toWrite = ""
+    case SCHEMATYPE_STRING:
+      toWrite = generateJavaStringSetter(subschema.(*StringSchema))
+    case SCHEMATYPE_INTEGER: fallthrough
+    case SCHEMATYPE_NUMBER:
+      toWrite = generateJavaNumericSetter(subschema.(NumericSchemaType))
+    case SCHEMATYPE_OBJECT:
+      toWrite = generateJavaObjectSetter(subschema.(*ObjectSchema))
+    case SCHEMATYPE_ARRAY:
+      toWrite = generateJavaArraySetter(subschema.(*ArraySchema))
+    }
+
+    toWrite = fmt.Sprintf("\n\t\t%s = value;", properName)
+    ret.WriteString(toWrite)
+
+    ret.WriteString("\n\t}\n")
+  }
+
+  return ret.String()
+}
+
+func generateJavaRangeCheck(value interface{}, reference string, format string, exclusive bool, comparator, exclusiveComparator string) string {
+
+	var ret bytes.Buffer
+	var toWrite, compareString string
+
+	if(exclusive) {
+		compareString = exclusiveComparator
+	} else {
+		compareString = comparator
+	}
+
+	toWrite = "\n\tif("+ reference +" " + compareString + " " +format+ ")\n\t{"
+	toWrite = fmt.Sprintf(toWrite, value)
+	ret.WriteString(toWrite)
+
+	toWrite = fmt.Sprintf("\n\t\tthrow new Exception(\"Property '\"+value+\"' was out of range.\")\n\t}\n")
+	ret.WriteString(toWrite)
+
+	return ret.String()
+}
+
+func generateJavaStringSetter(schema *StringSchema) string {
+
+  var ret bytes.Buffer
+
+  // TODO: null check
+  // TODO: length checks
+  // TODO: pattern checks
+  return ret.String()
+}
+
+func generateJavaNumericSetter(schema NumericSchemaType) string {
+
+  var ret bytes.Buffer
+
+  // TODO: null check
+  // TODO: min/max checks
+  // TODO: multiple checks
+  return ret.String()
+}
+
+func generateJavaObjectSetter(schema *ObjectSchema) string {
+
+  var ret bytes.Buffer
+
+  // TODO: null check
+  return ret.String()
+}
+
+func generateJavaArraySetter(schema *ArraySchema) string {
+
+  var ret bytes.Buffer
+
+  // TODO: null check
+  // TODO: min/max length checks
   return ret.String()
 }
 
 func generateJavaTypeForSchema(subschema TypeSchema) string {
-  return "String"
+
+  switch(subschema.GetSchemaType()) {
+  case SCHEMATYPE_NUMBER: return "double"
+  case SCHEMATYPE_INTEGER: return "int"
+  case SCHEMATYPE_ARRAY: return subschema.GetTitle() + "[]"
+  case SCHEMATYPE_OBJECT: return subschema.GetTitle()
+  case SCHEMATYPE_STRING: return "String"
+  case SCHEMATYPE_BOOLEAN: return "boolean"
+  }
+
+  return "Object"
 }
