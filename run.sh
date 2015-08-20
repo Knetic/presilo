c9ff3f09e689bb2280aa8e95bd37a249653d13a2
George Lester <glester491@gmail.com>
2015-08-20 10:11:54 -0700

    Ensured go and C# build as libraries, to prevent errors.

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 make
-./.output/presilo -o ./.temp/go/ -l go -m main ./samples/wardrobe.json
-./.output/presilo -o ./.temp/js/ -l js -m main ./samples/wardrobe.json
+./.output/presilo -o ./.temp/go/ -l go -m foo ./samples/wardrobe.json
+./.output/presilo -o ./.temp/js/ -l js -m foo ./samples/wardrobe.json
 ./.output/presilo -o ./.temp/java/ -l java -m foo ./samples/wardrobe.json
 ./.output/presilo -o ./.temp/cs/ -l cs -m foo ./samples/wardrobe.json
 
-./.output/presilo -o ./.temp/go/ -l go -m main ./samples/person.json
-./.output/presilo -o ./.temp/js/ -l js -m main ./samples/person.json
+./.output/presilo -o ./.temp/go/ -l go -m foo ./samples/person.json
+./.output/presilo -o ./.temp/js/ -l js -m foo ./samples/person.json
 ./.output/presilo -o ./.temp/java/ -l java -m foo ./samples/person.json
 ./.output/presilo -o ./.temp/cs/ -l cs -m foo ./samples/person.json
 
-./.output/presilo -o ./.temp/go/ -l go -m main ./samples/car.json
-./.output/presilo -o ./.temp/js/ -l js -m main ./samples/car.json
+./.output/presilo -o ./.temp/go/ -l go -m foo ./samples/car.json
+./.output/presilo -o ./.temp/js/ -l js -m foo ./samples/car.json
 ./.output/presilo -o ./.temp/java/ -l java -m foo ./samples/car.json
 ./.output/presilo -o ./.temp/cs/ -l cs -m foo ./samples/car.json
 
@@ -23,5 +23,5 @@ pushd ./.temp/java
 javac $(find . -name "*.java")
 popd
 pushd ./.temp/cs/
-mcs $(find . -name "*.cs")
+mcs -T:library $(find . -name "*.cs")
 popd
