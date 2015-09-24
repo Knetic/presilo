7ce51e75da4d92e24bacc5d3ffd68fc8152dda1a
George Lester <glester491@gmail.com>
2015-09-23 17:06:06 -0700

    Converted mysql to use new indentation

--- src/presilo/codegen_mysql.go
+++ src/presilo/codegen_mysql.go
@@ -1,8 +1,6 @@
 package presilo
 
 import (
-	"bytes"
-	"strings"
 	"fmt"
 )
 
@@ -21,26 +19,26 @@ import (
 */
 func GenerateMySQL(schema *ObjectSchema, module string) string {
 
-	var ret bytes.Buffer
+	var buffer *BufferedFormatString
 
-	ret.WriteString(generateMysqlCreate(schema, module))
+	buffer = NewBufferedFormatString("\t")
+	generateMysqlCreate(schema, module, buffer)
 
-	return ret.String()
+	return buffer.String()
 }
 
-func generateMysqlCreate(schema *ObjectSchema, module string) string {
+func generateMysqlCreate(schema *ObjectSchema, module string, buffer *BufferedFormatString) {
 
-  var ret bytes.Buffer
-	var columns []string
-  var toWrite string
-  var required bool
+  var required, firstProperty bool
 
-  toWrite = fmt.Sprintf("USE %s;\nGO;\n", module)
-  ret.WriteString(toWrite)
+  buffer.Printf("USE %s;\nGO;\n", module)
 
   // create table
-  toWrite = fmt.Sprintf("CREATE TABLE dbo.%s\n(\n", schema.GetTitle())
-  ret.WriteString(toWrite)
+  buffer.Printf("CREATE TABLE dbo.%s\n(", schema.GetTitle())
+  buffer.AddIndentation(1)
+	buffer.Printf("\n")
+
+	firstProperty = true
 
   for propertyName, subschema := range schema.Properties {
 
@@ -54,191 +52,163 @@ func generateMysqlCreate(schema *ObjectSchema, module string) string {
       }
     }
 
-    switch subschema.GetSchemaType() {
-		case SCHEMATYPE_BOOLEAN:
-			toWrite = generateMySQLBoolColumn(propertyName, required, subschema.(*BooleanSchema))
-		case SCHEMATYPE_STRING:
-			toWrite = generateMySQLStringColumn(propertyName, required, subschema.(*StringSchema))
-		case SCHEMATYPE_INTEGER:
-			toWrite = generateMySQLIntegerColumn(propertyName, required, subschema.(*IntegerSchema))
-		case SCHEMATYPE_NUMBER:
-			toWrite = generateMySQLNumberColumn(propertyName, required, subschema.(*NumberSchema))
-		case SCHEMATYPE_OBJECT:
-			toWrite = generateMySQLReferenceColumn(propertyName, required, subschema.(*ObjectSchema))
-		case SCHEMATYPE_ARRAY:
-			toWrite = generateMySQLArrayColumn(propertyName, required, subschema.(*ArraySchema))
+		if(firstProperty) {
+			firstProperty = false
+		} else {
+			buffer.Print(",\n")
 		}
 
-		columns = append(columns, toWrite)
+    switch subschema.GetSchemaType() {
+		case SCHEMATYPE_BOOLEAN:
+			generateMySQLBoolColumn(propertyName, required, subschema.(*BooleanSchema), buffer)
+		case SCHEMATYPE_STRING:
+			generateMySQLStringColumn(propertyName, required, subschema.(*StringSchema), buffer)
+		case SCHEMATYPE_INTEGER:
+			generateMySQLIntegerColumn(propertyName, required, subschema.(*IntegerSchema), buffer)
+		case SCHEMATYPE_NUMBER:
+			generateMySQLNumberColumn(propertyName, required, subschema.(*NumberSchema), buffer)
+		case SCHEMATYPE_OBJECT:
+			generateMySQLReferenceColumn(propertyName, required, subschema.(*ObjectSchema), buffer)
+		case SCHEMATYPE_ARRAY:
+			generateMySQLArrayColumn(propertyName, required, subschema.(*ArraySchema), buffer)
+		}
   }
 
-	ret.WriteString(strings.Join(columns, ",\n"))
-  ret.WriteString("\n);")
+	buffer.AddIndentation(-1)
+  buffer.Print("\n);")
 
   // execution.
-  ret.WriteString("\nGO;\n\n")
-  return ret.String()
+  buffer.Print("\nGO;\n\n")
 }
 
-func generateMySQLBoolColumn(name string, required bool, schema *BooleanSchema) string {
+func generateMySQLBoolColumn(name string, required bool, schema *BooleanSchema, buffer *BufferedFormatString) {
 
-  var ret bytes.Buffer
-  var toWrite string
-
-  toWrite = fmt.Sprintf("\t%s bit", name)
-  ret.WriteString(toWrite)
+  buffer.Printf("\t%s bit", name)
+  buffer.AddIndentation(1)
 
   if(required) {
-    ret.WriteString(generateMySQLRequiredConstraint())
+    generateMySQLRequiredConstraint(buffer)
   }
 
-	toWrite = fmt.Sprintf("\n\t\tCHECK(%s = 0 OR %s = 1)", name, name)
-	ret.WriteString(toWrite)
-
-  return ret.String()
+	buffer.Printf("\nCHECK(%s = 0 OR %s = 1)", name, name)
+	buffer.AddIndentation(-1)
 }
 
-func generateMySQLStringColumn(name string, required bool, schema *StringSchema) string {
+func generateMySQLStringColumn(name string, required bool, schema *StringSchema, buffer *BufferedFormatString) {
 
-  var ret bytes.Buffer
-  var toWrite string
-
-  toWrite = fmt.Sprintf("\t%s nvarchar(128)", name)
-  ret.WriteString(toWrite)
+  buffer.Printf("%s nvarchar(128)", name)
+  buffer.AddIndentation(1)
 
   if(required) {
-    ret.WriteString(generateMySQLRequiredConstraint())
+    generateMySQLRequiredConstraint(buffer)
   }
 
 	if schema.MinLength != nil {
-		ret.WriteString(generateMySQLRangeCheck(*schema.MinLength, name, "%d", false, "<", ""))
+		generateMySQLRangeCheck(*schema.MinLength, name, "%d", false, "<", "", buffer)
 	}
 
 	if schema.MaxLength != nil {
-		ret.WriteString(generateMySQLRangeCheck(*schema.MaxLength, name, "%d", false, ">", ""))
+		generateMySQLRangeCheck(*schema.MaxLength, name, "%d", false, ">", "", buffer)
 	}
 
 	if(schema.Enum != nil) {
-		ret.WriteString(generateMySQLEnumCheck(schema, schema.GetEnum(), "'", "'"))
+		generateMySQLEnumCheck(schema, schema.GetEnum(), "'", "'", buffer)
 	}
 
-  return ret.String()
+	buffer.AddIndentation(-1)
 }
 
-func generateMySQLIntegerColumn(name string, required bool, schema *IntegerSchema) string {
+func generateMySQLIntegerColumn(name string, required bool, schema *IntegerSchema, buffer *BufferedFormatString) {
 
-  var ret bytes.Buffer
-  var toWrite string
-
-  toWrite = fmt.Sprintf("\t%s int", name)
-  ret.WriteString(toWrite)
+  buffer.Printf("%s int", name)
+  buffer.AddIndentation(1)
 
   if(required) {
-    ret.WriteString(generateMySQLRequiredConstraint())
+    generateMySQLRequiredConstraint(buffer)
   }
 
-	ret.WriteString(generateMySQLNumericConstraints(name, schema))
-  return ret.String()
+	generateMySQLNumericConstraints(name, schema, buffer)
+  buffer.AddIndentation(-1)
 }
 
-func generateMySQLNumberColumn(name string, required bool, schema *NumberSchema) string {
+func generateMySQLNumberColumn(name string, required bool, schema *NumberSchema, buffer *BufferedFormatString) {
 
-  var ret bytes.Buffer
-  var toWrite string
-
-  toWrite = fmt.Sprintf("\t%s float", name)
-  ret.WriteString(toWrite)
+  buffer.Printf("%s float", name)
+  buffer.AddIndentation(1)
 
   if(required) {
-    ret.WriteString(generateMySQLRequiredConstraint())
+    generateMySQLRequiredConstraint(buffer)
   }
 
-	ret.WriteString(generateMySQLNumericConstraints(name, schema))
-  return ret.String()
+	generateMySQLNumericConstraints(name, schema, buffer)
+	buffer.AddIndentation(-1)
 }
 
-func generateMySQLReferenceColumn(name string, required bool, schema *ObjectSchema) string {
+func generateMySQLReferenceColumn(name string, required bool, schema *ObjectSchema, buffer *BufferedFormatString) {
 
-  var ret bytes.Buffer
-  var toWrite string
-
-  toWrite = fmt.Sprintf("\t%s int(1)", name)
-  ret.WriteString(toWrite)
+  buffer.Printf("%s int(1)", name)
+  buffer.AddIndentation(1)
 
   if(required) {
-    ret.WriteString(generateMySQLRequiredConstraint())
+    generateMySQLRequiredConstraint(buffer)
   }
-  return ret.String()
+
+	buffer.AddIndentation(-1)
 }
 
-func generateMySQLArrayColumn(name string, required bool, schema *ArraySchema) string {
-
-  var ret bytes.Buffer
+func generateMySQLArrayColumn(name string, required bool, schema *ArraySchema, buffer *BufferedFormatString) {
 
   fmt.Println("Schema contains an array, which has no definite analogue in MySQL.")
-  return ret.String()
 }
 
-func generateMySQLRequiredConstraint() string {
-  return "\n\t\tNOT NULL"
+func generateMySQLRequiredConstraint(buffer *BufferedFormatString) {
+
+  buffer.Print("\nNOT NULL")
 }
 
 /*
 	Generates code which throws an error if the given [parameter]'s value is not contained in the given [validValues].
 */
-func generateMySQLEnumCheck(schema interface{}, enumValues []interface{}, prefix string, postfix string) string {
+func generateMySQLEnumCheck(schema interface{}, enumValues []interface{}, prefix string, postfix string, buffer *BufferedFormatString) {
 
-	var ret bytes.Buffer
-	var constraint string
 	var length int
 
 	length = len(enumValues)
 
 	if length <= 0 {
-		return ""
+		return
 	}
 
 	// write array of valid values
-	constraint = fmt.Sprintf("\n\t\tENUM(%s%v%s", prefix, enumValues[0], postfix)
-	ret.WriteString(constraint)
+	buffer.Printf("\nENUM(%s%v%s", prefix, enumValues[0], postfix)
 
 	for _, enumValue := range enumValues[1:length] {
-
-		constraint = fmt.Sprintf(",%s%v%s", prefix, enumValue, postfix)
-		ret.WriteString(constraint)
+		buffer.Printf(",%s%v%s", prefix, enumValue, postfix)
 	}
-	ret.WriteString(")")
 
-	return ret.String()
+	buffer.Print(")")
 }
 
-func generateMySQLNumericConstraints(name string, schema NumericSchemaType) string {
-
-  var ret bytes.Buffer
-  var toWrite string
+func generateMySQLNumericConstraints(name string, schema NumericSchemaType, buffer *BufferedFormatString) {
 
 	if schema.HasMinimum() {
-		ret.WriteString(generateMySQLRangeCheck(schema.GetMinimum(), "value", schema.GetConstraintFormat(), schema.IsExclusiveMinimum(), "<=", "<"))
+		generateMySQLRangeCheck(schema.GetMinimum(), "value", schema.GetConstraintFormat(), schema.IsExclusiveMinimum(), "<=", "<", buffer)
 	}
 
 	if schema.HasMaximum() {
-		ret.WriteString(generateMySQLRangeCheck(schema.GetMaximum(), "value", schema.GetConstraintFormat(), schema.IsExclusiveMaximum(), ">=", ">"))
+		generateMySQLRangeCheck(schema.GetMaximum(), "value", schema.GetConstraintFormat(), schema.IsExclusiveMaximum(), ">=", ">", buffer)
 	}
 
 	if schema.HasEnum() {
-		ret.WriteString(generateMySQLEnumCheck(schema, schema.GetEnum(), "", ""))
+		generateMySQLEnumCheck(schema, schema.GetEnum(), "", "", buffer)
 	}
 
 	if schema.HasMultiple() {
-
-		toWrite = fmt.Sprintf("\n\t\tCHECK(mod(%s, %v) = 0)", name, schema.GetMultiple())
-		ret.WriteString(toWrite)
+		buffer.Printf("\nCHECK(mod(%s, %v) = 0)", name, schema.GetMultiple())
 	}
-
-	return ret.String()
 }
 
-func generateMySQLRangeCheck(value interface{}, reference string, format string, exclusive bool, comparator, exclusiveComparator string) string {
+func generateMySQLRangeCheck(value interface{}, reference string, format string, exclusive bool, comparator, exclusiveComparator string, buffer *BufferedFormatString) {
 
 	var compareString string
 
@@ -248,5 +218,5 @@ func generateMySQLRangeCheck(value interface{}, reference string, format string,
 		compareString = comparator
 	}
 
-	return fmt.Sprintf("\n\t\tCHECK(%s %s %v)", reference, compareString, value)
+	buffer.Printf("\nCHECK(%s %s %v)", reference, compareString, value)
 }
